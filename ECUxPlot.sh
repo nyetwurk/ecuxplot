56f36df1f3457f12da6bb9644e64f579bb23fda6
nyet <nyet@32951a55-7e27-4d3b-b14c-4e1278f75193>
2008-11-25 00:14:36 +0000

    ooops. lets not check in something bad

--- ECUxPlot.sh
+++ ECUxPlot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-PROG=`readlink -z "$0" 2>/dev/null || true`
+PROG=`readlink -m "$0" 2>/dev/null || true`
 [ -z "$PROG" ] && PROG=$0
 DIR=`dirname "$PROG"`
 java -Dawt.useSystemAAFontSettings=on -jar $DIR/ECUxPlot-`cat $DIR/version.txt`.jar "$@"
