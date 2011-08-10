9cd372f40e8b2bc8da8b1dcefb9e4b6743290bc1
nyet <nyet@32951a55-7e27-4d3b-b14c-4e1278f75193>
2011-08-10 01:19:57 +0000

    ugh. for some reason xp needs this but win7 doesn't

--- scripts/Windows.mk
+++ scripts/Windows.mk
@@ -12,7 +12,7 @@ ECUxPlot-$(ECUXPLOT_VER).jar: ECUxPlot.MF $(EX_CLASSES)
 
 # unix launch4j requires full path to .xml
 ECUxPlot.exe: ECUxPlot-$(ECUXPLOT_VER).jar ECUxPlot.xml ECUxPlot.ico version.txt
-	$(LAUNCH4J) $(PWD)/ECUxPlot.xml
+	$(LAUNCH4J) "$(shell cygpath -w $(PWD)/ECUxPlot.xml)"
 
 $(INSTALLER): ECUxPlot.exe $(INSTALL_FILES) ECUxPlot.sh scripts/ECUxPlot.nsi
 	$(MAKENSIS) $(OPT_PRE)NOCD \
