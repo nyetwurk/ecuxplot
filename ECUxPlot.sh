bd2d5eb2d54b9ffd730b4f7be0424417748a5ba0
Nye Liu <nyet@nyet.org>
2013-01-19 16:03:47 -0800

    Handle paths better in cygwin

--- ECUxPlot.sh
+++ ECUxPlot.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 set -e
-PROG=`readlink -m "$0" 2>/dev/null || true`
+
+PROG=$(readlink -m "$0" 2>/dev/null || true)
 [ -z "$PROG" ] && PROG=$0
 DIR=`dirname "$PROG"`
 
 for i in build/version.txt version.txt; do
-    [ -r "$DIR/$i" ] && VER=`cat "$DIR/$i"`
+    [ -r "$DIR/$i" ] && VER=$(cat "$DIR/$i")
 done
 
-exec java -Dawt.useSystemAAFontSettings=on -jar "$DIR/ECUxPlot-$VER.jar" "$@"
+jar=$DIR/ECUxPlot-$VER.jar
+
+args=()
+if [ ! -z $(which cygpath) ]; then
+    jar=$(cygpath -w $jar)
+    for arg in "$@"; do
+	if [ -r "$arg" ]; then
+	    args+=($(cygpath -w "$arg"))
+	else
+	    args+=$arg
+	fi
+    done
+else
+    args=$@
+fi
+
+# echo exec java -Dawt.useSystemAAFontSettings=on -jar "$jar" "${args[@]}"
+exec java -Dawt.useSystemAAFontSettings=on -jar "$jar" "${args[@]}"
