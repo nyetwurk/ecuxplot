9914d38f391641173946e0a438a584e3d4b54435
sbloom82 <sbloom82@hotmail.com>
2014-03-17 22:27:30 -0500

    better work now

--- src/org/nyet/ecuxplot/AxisMenu.java
+++ src/org/nyet/ecuxplot/AxisMenu.java
@@ -24,7 +24,7 @@ public class AxisMenu extends JMenu {
      * 
      */
 	
-	public static boolean thisIsATest = false;
+	public static boolean thisIsATest1234 = false;
     private static final long serialVersionUID = 1L;
     private SubActionListener listener;
     private boolean radioButton;
